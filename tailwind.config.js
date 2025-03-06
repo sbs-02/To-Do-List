52c36b8b603783030509708d99ec6132808cfc42
sbs002-hub <dangolsaksham8@gmail.com>
2025-03-06 15:28:15 +0545

    add Tailwind CSS setup and implement basic To-Do app functionality

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-135": "linear-gradient(135deg, #153677, #4e085f)",
+      },
+    },
+
+    plugins: [],
+  },
+};
